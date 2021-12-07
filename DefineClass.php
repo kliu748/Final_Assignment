bf877e89e8d94daaf77d93339f0843339937ae1e
kliu748 <79317734+kliu748@users.noreply.github.com>
2021-12-07 05:28:28 -0500

    Add files via upload

---
+++ DefineClass.php
@@ -0,0 +1,10 @@
+<?php
+class database{
+    private $host = 'localhost';
+    private $db_name = "Data Table";
+    private $username = 'root';
+    private $password = '1234';
+    private $conn;
+
+}
+?>
